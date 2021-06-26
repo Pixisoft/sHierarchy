9c509a5306fd90990b6be39c2a1a4da541bd8852
JenChieh <jcs090218@gmail.com>
2021-06-26 12:16:08 +0800

    Add publish

---
+++ Publish/Assets/sHierarchy/Demo/Scripts/Log/Test_LogError.cs
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Pixisoft Corporations. All rights reserved.
+ * 
+ * Licensed under the Source EULA. See COPYING in the asset root for license informtaion.
+ */
+using UnityEngine;
+
+namespace sHierarchy.Test
+{
+    public class Test_LogError : MonoBehaviour
+    {
+        /* Variables */
+
+        /* Setter & Getter */
+
+        /* Functions */
+
+        private void Start()
+        {
+            Debug.LogError("This is a log error");
+        }
+    }
+}
